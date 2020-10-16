f3f34be59b0dcf2beb65e20d7d2919531e498f29
Brennan O'Connor <bo92@cornell.edu>
2020-10-16 19:40:23 -0400

    started adding some tests for card

--- blackjack/card.ml
+++ blackjack/card.ml
@@ -22,6 +22,9 @@ type value =
 
 type card = {suit: suit; value: value}
 
+let create_card suit value = 
+  {suit: suit; value: value}
+
 let get_suit (c : card) = 
   c.suit
 
@@ -54,3 +57,6 @@ let int_of_value (c : card) =
   | Queen -> 10
   | King -> 10
   | Ace -> 1
+
+let string_of_card (c : card) = 
+  string_of_int(int_of_value c) ^ " of " ^ string_of_suit c
--- blackjack/deck_test.ml
+++ blackjack/deck_test.ml
@@ -1,8 +1,56 @@
 open OUnit2
+open Deck
+open Card
 
+(** [cmp_set_like_lists lst1 lst2] compares two lists to see whether
+    they are equivalent set-like lists.  That means checking two things.
+    First, they must both be {i set-like}, meaning that they do not
+    contain any duplicates.  Second, they must contain the same elements,
+    though not necessarily in the same order. *)
+let cmp_set_like_lists lst1 lst2 =
+  let uniq1 = List.sort_uniq compare lst1 in
+  let uniq2 = List.sort_uniq compare lst2 in
+  List.length lst1 = List.length uniq1
+  &&
+  List.length lst2 = List.length uniq2
+  &&
+  uniq1 = uniq2
+
+(** [pp_string s] pretty-prints string [s]. *)
+let pp_string s = "\"" ^ s ^ "\""
+
+(** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt]
+    to pretty-print each element of [lst]. *)
+let pp_list pp_elt lst =
+  let pp_elts lst =
+    let rec loop n acc = function
+      | [] -> acc
+      | [h] -> acc ^ pp_elt h
+      | h1 :: (h2 :: t as t') ->
+        if n = 100 then acc ^ "..."  (* stop printing long list *)
+        else loop (n + 1) (acc ^ (pp_elt h1) ^ "; ") t'
+    in loop 0 "" lst
+  in "[" ^ pp_elts lst ^ "]"
+
+(** [starting_room_test name input expected_output] constructs an OUnit
+    test named [name] that asserts the quality of [expected_output]
+    with [starting_room input]. *)
+let suit_test 
+    (name : string) 
+    (suit) (value)
+    (expected_output) : test = 
+  name >:: (fun _ -> 
+      (* the [printer] tells OUnit how to convert the output to a string *)
+      assert_equal expected_output (create_card suit value))
+
+let card_tests =
+  [
+
+  ]
 
 let tests =
   "test suite for Blackjack"  >::: List.flatten [
+    card_tests;
 
   ]
 
