78a90ad16ed87e8dd2ccaaa3253d2f28c90e1a2a
awh76 <awh76@cornell.edu>
2020-10-18 20:20:59 -0400

    init state

--- blackjack/state.ml
+++ blackjack/state.ml
@@ -1,3 +1,13 @@
 open Player 
 open Card 
 open Deck 
+
+type state = {deck: card list; players: player list; cur_rotation: player list}
+
+(** [init_game_state player_names] returns a game state with as many players
+    as are in [player_names] plus the house *)
+let init_game_state player_names = 
+  let deck = shuffle create_standard_deck in 
+  let players = (List.map (fun x -> {name=x; hand=[]}) player_names) in 
+  let players_house = {name="HOUSE"; hand=[]} :: players in 
+  {deck=deck; players=players; cur_rotation=players_house}
